4d53bed300d10d3de028fdaeb8908ab969bb12e9
DYMripper <anthony293255@gmail.com>
2026-08-24 18:48:04 +0200

    Update

--- ffmpeg/worker.js
+++ ffmpeg/worker.js
@@ -1,141 +1,303 @@
 /// <reference no-default-lib="true" />
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
-import { CORE_URL, FFMessageType } from "./const.js";
-import { ERROR_UNKNOWN_MESSAGE_TYPE, ERROR_NOT_LOADED, ERROR_IMPORT_FAILURE, } from "./errors.js";
-let ffmpeg;
-const load = async ({ coreURL: _coreURL, wasmURL: _wasmURL, workerURL: _workerURL, }) => {
+
+// FFmpeg Classic Worker
+// 不使用 ES Module import。
+// 必须通过 Classic Worker 启动：
+// new Worker('/ffmpeg/worker.js')
+
+const CORE_URL =
+    "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js";
+
+const FFMessageType = {
+    LOAD: "LOAD",
+    EXEC: "EXEC",
+    WRITE_FILE: "WRITE_FILE",
+    READ_FILE: "READ_FILE",
+    DELETE_FILE: "DELETE_FILE",
+    RENAME: "RENAME",
+    CREATE_DIR: "CREATE_DIR",
+    LIST_DIR: "LIST_DIR",
+    DELETE_DIR: "DELETE_DIR",
+    ERROR: "ERROR",
+    DOWNLOAD: "DOWNLOAD",
+    PROGRESS: "PROGRESS",
+    LOG: "LOG",
+    MOUNT: "MOUNT",
+    UNMOUNT: "UNMOUNT",
+};
+
+const ERROR_UNKNOWN_MESSAGE_TYPE =
+    new Error("unknown message type");
+
+const ERROR_NOT_LOADED =
+    new Error("ffmpeg is not loaded, call `await ffmpeg.load()` first");
+
+let ffmpeg = null;
+
+const load = async ({
+    coreURL: _coreURL,
+    wasmURL: _wasmURL,
+    workerURL: _workerURL,
+}) => {
     const first = !ffmpeg;
 
-    if (!_coreURL)
+    if (!_coreURL) {
         _coreURL = CORE_URL;
+    }
 
+    // Classic Worker 可以使用 importScripts()
     importScripts(_coreURL);
-    
+
     const coreURL = _coreURL;
-    const wasmURL = _wasmURL ? _wasmURL : _coreURL.replace(/.js$/g, ".wasm");
+
+    const wasmURL = _wasmURL
+        ? _wasmURL
+        : _coreURL.replace(/\.js$/g, ".wasm");
+
     const workerURL = _workerURL
         ? _workerURL
-        : _coreURL.replace(/.js$/g, ".worker.js");
+        : _coreURL.replace(/\.js$/g, ".worker.js");
+
+    if (typeof self.createFFmpegCore !== "function") {
+        throw new Error(
+            "createFFmpegCore 不存在，ffmpeg-core.js 加载失败"
+        );
+    }
+
     ffmpeg = await self.createFFmpegCore({
-        // Fix `Overload resolution failed.` when using multi-threaded ffmpeg-core.
-        // Encoded wasmURL and workerURL in the URL as a hack to fix locateFile issue.
-        mainScriptUrlOrBlob: `${coreURL}#${btoa(JSON.stringify({ wasmURL, workerURL }))}`,
+        mainScriptUrlOrBlob:
+            `${coreURL}#${btoa(
+                JSON.stringify({
+                    wasmURL,
+                    workerURL,
+                })
+            )}`,
     });
-    ffmpeg.setLogger((data) => self.postMessage({ type: FFMessageType.LOG, data }));
-    ffmpeg.setProgress((data) => self.postMessage({
-        type: FFMessageType.PROGRESS,
-        data,
-    }));
+
+    ffmpeg.setLogger((data) => {
+        self.postMessage({
+            type: FFMessageType.LOG,
+            data,
+        });
+    });
+
+    ffmpeg.setProgress((data) => {
+        self.postMessage({
+            type: FFMessageType.PROGRESS,
+            data,
+        });
+    });
+
     return first;
 };
+
 const exec = ({ args, timeout = -1 }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.setTimeout(timeout);
+
     ffmpeg.exec(...args);
+
     const ret = ffmpeg.ret;
+
     ffmpeg.reset();
+
     return ret;
 };
+
 const writeFile = ({ path, data }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.writeFile(path, data);
+
     return true;
 };
-const readFile = ({ path, encoding }) => ffmpeg.FS.readFile(path, { encoding });
-// TODO: check if deletion works.
+
+const readFile = ({ path, encoding }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
+    return ffmpeg.FS.readFile(path, { encoding });
+};
+
 const deleteFile = ({ path }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.unlink(path);
+
     return true;
 };
+
 const rename = ({ oldPath, newPath }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.rename(oldPath, newPath);
+
     return true;
 };
-// TODO: check if creation works.
+
 const createDir = ({ path }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.mkdir(path);
+
     return true;
 };
+
 const listDir = ({ path }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     const names = ffmpeg.FS.readdir(path);
     const nodes = [];
+
     for (const name of names) {
         const stat = ffmpeg.FS.stat(`${path}/${name}`);
-        const isDir = ffmpeg.FS.isDir(stat.mode);
-        nodes.push({ name, isDir });
+
+        nodes.push({
+            name,
+            isDir: ffmpeg.FS.isDir(stat.mode),
+        });
     }
+
     return nodes;
 };
-// TODO: check if deletion works.
+
 const deleteDir = ({ path }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.rmdir(path);
+
     return true;
 };
+
 const mount = ({ fsType, options, mountPoint }) => {
-    const str = fsType;
-    const fs = ffmpeg.FS.filesystems[str];
-    if (!fs)
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
+    const fs = ffmpeg.FS.filesystems[fsType];
+
+    if (!fs) {
         return false;
-    ffmpeg.FS.mount(fs, options, mountPoint);
+    }
+
+    ffmpeg.FS.mount(
+        fs,
+        options,
+        mountPoint
+    );
+
     return true;
 };
+
 const unmount = ({ mountPoint }) => {
+    if (!ffmpeg) {
+        throw ERROR_NOT_LOADED;
+    }
+
     ffmpeg.FS.unmount(mountPoint);
+
     return true;
 };
-self.onmessage = async ({ data: { id, type, data: _data }, }) => {
-    const trans = [];
-    let data;
+
+// Worker 消息入口
+self.onmessage = async ({ data: message }) => {
+    const { id, type, data } = message;
+
+    let result;
+
     try {
-        if (type !== FFMessageType.LOAD && !ffmpeg)
-            throw ERROR_NOT_LOADED; // eslint-disable-line
         switch (type) {
             case FFMessageType.LOAD:
-                data = await load(_data);
+                result = await load(data);
                 break;
+
             case FFMessageType.EXEC:
-                data = exec(_data);
+                result = exec(data);
                 break;
+
             case FFMessageType.WRITE_FILE:
-                data = writeFile(_data);
+                result = writeFile(data);
                 break;
+
             case FFMessageType.READ_FILE:
-                data = readFile(_data);
+                result = readFile(data);
                 break;
+
             case FFMessageType.DELETE_FILE:
-                data = deleteFile(_data);
+                result = deleteFile(data);
                 break;
+
             case FFMessageType.RENAME:
-                data = rename(_data);
+                result = rename(data);
                 break;
+
             case FFMessageType.CREATE_DIR:
-                data = createDir(_data);
+                result = createDir(data);
                 break;
+
             case FFMessageType.LIST_DIR:
-                data = listDir(_data);
+                result = listDir(data);
                 break;
+
             case FFMessageType.DELETE_DIR:
-                data = deleteDir(_data);
+                result = deleteDir(data);
                 break;
+
             case FFMessageType.MOUNT:
-                data = mount(_data);
+                result = mount(data);
                 break;
+
             case FFMessageType.UNMOUNT:
-                data = unmount(_data);
+                result = unmount(data);
                 break;
+
             default:
                 throw ERROR_UNKNOWN_MESSAGE_TYPE;
         }
-    }
-    catch (e) {
+    } catch (error) {
         self.postMessage({
             id,
             type: FFMessageType.ERROR,
-            data: e.toString(),
+            data: error instanceof Error
+                ? `${error.name}: ${error.message}`
+                : String(error),
         });
+
         return;
     }
-    if (data instanceof Uint8Array) {
-        trans.push(data.buffer);
+
+    // readFile() 返回 Uint8Array 时，
+    // 把底层 ArrayBuffer 作为 transferable 传回主线程。
+    const trans = [];
+
+    if (result instanceof Uint8Array) {
+        trans.push(result.buffer);
     }
-    self.postMessage({ id, type, data }, trans);
+
+    self.postMessage(
+        {
+            id,
+            type,
+            data: result,
+        },
+        trans
+    );
 };
